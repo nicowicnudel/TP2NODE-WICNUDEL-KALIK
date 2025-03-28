f1c20f7d84b0c50cfab4d89f356c2e38395366ee
Android Studio <devandroid@ort>
2025-03-28 10:33:09 -0300

    ej03 terminado y ej04 empezando

---
+++ src/ej04.js
@@ -0,0 +1,7 @@
+import FS from 'fs';
+const ARCHIVO_ENTRADA = "./archivo-entrada.txt";
+const ARCHIVO_SALIDA = "./archivo-salida.txt";
+console.clear();
+copiar(ARCHIVO_ENTRADA, ARCHIVO_SALIDA);
+function copiar(origen, destino){
+}
