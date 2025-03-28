bae8340db44ce9727aa19b09e3b39e5f9b484d41
Android Studio <devandroid@ort>
2025-03-28 09:18:18 -0300

    ej 01

--- src/ej01.js
+++ src/ej01.js
@@ -1,1 +1,14 @@
-console.log("hola dai")
+let textoEntrada01 = "Escuela", textoEntrada02 = "ORT";
+let textoSalida = "";
+textoSalida = concatInvert(textoEntrada01, textoEntrada02);
+console.clear(); // Borra la pantalla en la consola.
+console.log(`Textos de Entrada: "${textoEntrada01}" y "${textoEntrada02}"`);
+console.log(`Texto de Salida: "${textoSalida}"`);
+function concatInvert (textoEntrada01, textoEntrada02){
+let returnValue = '';
+let concatenado = textoEntrada01.concat(textoEntrada02)
+let array = concatenado.split('')
+let reverse = array.reverse()
+returnValue = reverse.join('');
+return returnValue;
+}
