aa3b3d17c7a9f08b0e02aaad83293be5c53e03bf
Android Studio <devandroid@ort>
2025-03-31 11:34:47 -0300

    ej 04 termiando

--- src/ej04.js
+++ src/ej04.js
@@ -1,7 +1,10 @@
-import FS from 'fs';
+import fs from "fs";
+import { callbackify } from "util";
 const ARCHIVO_ENTRADA = "./archivo-entrada.txt";
 const ARCHIVO_SALIDA = "./archivo-salida.txt";
 console.clear();
 copiar(ARCHIVO_ENTRADA, ARCHIVO_SALIDA);
-function copiar(origen, destino){
+function copiar(origen, destino) {
+  fs.copyFile(origen, destino, ()=> console.log ("aguante stolar"));
+
 }
