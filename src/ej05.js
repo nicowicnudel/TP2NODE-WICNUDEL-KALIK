d57dba0c6bd7a52676037acbe89c57b89bf6d071
Frantag15 <48183956@ort>
2025-04-01 10:34:48 -0300

    ej 05 terminado

--- src/ej05.js
+++ src/ej05.js
@@ -1,13 +1,13 @@
-let miUrl    = null;
-let miObjeto = null;
-miUrl    = 'http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo';
-miObjeto = parsearUrl (miUrl);
-console.log(miObjeto);
-function parsearUrl(laURL){
-  console.log(new URL (laURL))
-  URL.host= URL.host;
-  URL.pathname =URL.pathname;
-  URL.parametros= URLSearchParams
- return host, pathname, parametros;
+let miUrl = 'http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo';
+let miObjeto = parsearUrl(miUrl);
+function parsearUrl(laURL) {
+  let urlObjeto = new URL(laURL);
+  return {
+      host: urlObjeto.origin,
+      pathname: urlObjeto.pathname,
+      parametros: Object.fromEntries(urlObjeto.searchParams.entries())
+  };
 }
+console.log(miObjeto);
+
 
